58b80f2358cbc9a6c87f20fa8886dbb4927d3704
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-26 13:19:33 +0700

    Implement useReducer and fixing bug

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,7 +2,9 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  return state + action;
+  if (action.type === "inc") return state + 1;
+  if (action.type === "dec") return state - 1;
+  if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
@@ -15,18 +17,19 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch(-1);
+    dispatch({ type: "dec" });
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch(1);
+    dispatch({ type: "inc" });
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
+    dispatch({ type: "setCount" });
     // setCount(Number(e.target.value));
   };
 
