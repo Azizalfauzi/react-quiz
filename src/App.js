9d6c44139703cb02a849beb945426cb10f69ee0b
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-06 05:29:35 +0700

    create useReducer in app component and adding from logic to get api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { useReducer } from "react";
 import Header from "./Header";
 import Main from "./main";
 import { useEffect } from "react";
 
+const initialState = {
+  questions: [],
+  // loading,error,ready,active,finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispacth] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => dispacth({ type: "dataReceived", payload: data }))
       .catch((err) => console.log(err));
   }, []);
 
