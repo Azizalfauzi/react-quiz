e998ef636117d2534355843a59c1450576ff1352
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-06 05:21:24 +0700

    adding json-server package and create us effect to fecth data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import Header from "./Header";
 import Main from "./main";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="app">
       <Header />
