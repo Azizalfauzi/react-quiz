a2fbaa46d2a389cd4c7fb559851315e45748dd7e
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-06 13:09:33 +0700

    Move file component in component folder and create Options component to load data question and pass data there

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,8 +1,11 @@
+import Option from "./Options";
+
 export default function Question({ question }) {
   console.log(question);
   return (
     <div>
       <h4>{question.question}</h4>
+      <Option question={question} />
     </div>
   );
 }
