2976925edf682e47b3dab1433384bacac2028261
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-09 19:46:21 +0700

    adding condition in button after click in option component

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,7 +3,9 @@ export default function Options({ question, answer, dispatch }) {
     <div className="options">
       {question.options.map((option, index) => (
         <button
-          className="btn btn-option"
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            index === question.correctOption ? "correct" : "wrong"
+          }`}
           key={option}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
         >
